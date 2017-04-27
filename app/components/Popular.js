7d763d0eb22d490e5b5619e0b9ad277935b52cc7
ejarquin <ejarquin@thedataage.com>
2017-04-27 17:03:54 -0600

    hice que se mostraran los repositorios de github según el lenguaje

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -24,6 +24,44 @@ function SelectLanguage (props) {
 	)
 }
 
+function RepoGrid (props) {
+	return (
+		<ul className="popular-list">
+			{props.repos.map(function (repo, index) {
+				return (
+					<li key={repo.name} className='popular-item'>
+						<div className='popular-rank'>
+							#{index + 1}
+						</div>
+						<ul className="space-list-item">
+							<li>
+								<img 
+									className="avatar"
+									src={repo.owner.avatar_url}
+									alt={'Avatar for ' + repo.owner.login}
+								/>
+							</li>
+							<li>
+								<a href={repo.html_url}>{repo.name}</a>
+							</li>
+							<li>
+								@{repo.owner.login}
+							</li>
+							<li>
+								{repo.stargazers_count} stars
+							</li>
+						</ul>
+				</li>
+				)
+			})}
+		</ul>
+	)
+}
+
+RepoGrid.propTypes = {
+	repos: PropTypes.array.isRequired,
+}
+
 SelectLanguage.propTypes = {
 	selectedLanguage: PropTypes.string.isRequired,
 	onSelect: PropTypes.func.isRequired
@@ -38,7 +76,7 @@ class Popular extends React.Component {
 			}
 			this.updateLanguage = this.updateLanguage.bind(this)
 	}
-	
+
 	componentDidMount () {
 		this.updateLanguage(this.state.selectedLanguage)
 	}
@@ -60,13 +98,19 @@ class Popular extends React.Component {
 	}
 
 	render() {
+		console.log(this.state.repos)
 		return (
 			<div>
 				<SelectLanguage 
 					selectedLanguage={this.state.selectedLanguage}
 					onSelect={this.updateLanguage}
 				/>
-				{JSON.stringify(this.state.repos, 2, null)}
+				{ 
+					!this.state.repos ? 
+						'LOADING...'
+					:
+					 <RepoGrid repos={this.state.repos} />
+				}
 			</div>
 		)
 	}
